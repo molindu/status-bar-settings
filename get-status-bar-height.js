18a04f9e9ecbd8aa656514df001201624a59e46b
Molindu Yasoda <54410056+molindu@users.noreply.github.com>
2023-10-17 11:58:02 +0530

    Update get-status-bar-height.js

--- get-status-bar-height.js
+++ get-status-bar-height.js
@@ -1,16 +1,23 @@
-import { useEffect, useState } from 'react';
-import { StatusBar, StatusBarManager } from 'react-native';
+// import { useEffect, useState } from 'react';
+// import { StatusBar, StatusBarManager } from 'react-native';
 
-function YourComponent() {
-  const [statusBarHeight, setStatusBarHeight] = useState(0);
+// function YourComponent() {
+//   const [statusBarHeight, setStatusBarHeight] = useState(0);
 
-  useEffect(() => {
-    StatusBarManager.getHeight((statusBarHeight) => {
-      setStatusBarHeight(statusBarHeight.height);
-    });
-  }, []);
+//   useEffect(() => {
+//     StatusBarManager.getHeight((statusBarHeight) => {
+//       setStatusBarHeight(statusBarHeight.height);
+//     });
+//   }, []);
 
-  return (
-    // Your component code
-  );
-}
+//   return (
+//     // Your component code
+//   );
+// }
+import { StatusBar } from 'react-native';
+
+// Get the status bar height
+const statusBarHeight = StatusBar.currentHeight;
+
+console.log("Status Bar Height:", statusBarHeight);
+
